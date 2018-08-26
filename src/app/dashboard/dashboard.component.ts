03a6b2dc6e86bf0565b753bf26ed53750d851637
jordanSu <newjordansu1126@gmail.com>
2018-08-26 21:23:15 +0800

    增加鍵盤操作, 臉部出現會增加係數,總體係數由兩特徵加權而得

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,15 @@
-import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from "@angular/core";
 import * as Chartist from "chartist";
 import { ChartComponent } from "angular2-chartjs";
-import Chart from 'chart.js';
+// import Chart from 'chart.js';
 import '../../assets/js/tracking-min.js';
 import '../../assets/js/face-min.js';
+
 declare var tracking: any;
+declare var Chart: any;
+
+const BAR_MIN = 10;
+const BAR_MAX = 95;
 
 @Component({
   moduleId: module.id,
@@ -24,13 +29,20 @@ export class DashboardComponent implements OnInit {
   // 甜甜圈圖: 總體可疑係數
   @ViewChild("ct1") chart1: ChartComponent;
 
+  @ViewChild("mycanvas") canvas: ElementRef;
+
   @ViewChild("test") donut: ElementRef;
+
+  context: CanvasRenderingContext2D;
 
   isSuccessAccessVideo = true;
 
   errorMessage: string = null;
 
-  totalPercentage = 40;
+  // 特徵係數初始化
+  facePercentage = 30;
+  behaviorPercentage = 20;
+  totalPercentage = this.facePercentage + this.behaviorPercentage;
 
   // 現在時間
   currentTime: Date;
@@ -41,8 +53,8 @@ export class DashboardComponent implements OnInit {
     labels: ["臉部", "行為"],
     datasets: [
       {
-        label: "可疑指數",
-        data: [47, 66],
+        label: "特徵係數",
+        data: [this.facePercentage, this.behaviorPercentage],
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
@@ -88,8 +100,8 @@ export class DashboardComponent implements OnInit {
 
     // These labels appear in the legend and in the tooltips when hovering different arcs
     labels: [
-        'Red',
-        'Yellow',
+        '可疑係數',
+        '',
     ]
   };
 
@@ -99,14 +111,6 @@ export class DashboardComponent implements OnInit {
     legend: {
       display: false
     },
-    elements: {
-      center: {
-        text: '60%',
-        color: '#36A2EB',
-        fontStyle: 'Helvetica',
-        sidePadding: 15 // Default 20 (as a percentage)
-      }
-    },
     // donut 厚度
     cutoutPercentage: 70,
     // segmentShowStroke : false,
@@ -114,9 +118,11 @@ export class DashboardComponent implements OnInit {
   };
 
   updateDonutChart() {
+    this.totalPercentage = this.facePercentage * 0.5 + this.behaviorPercentage * 0.5;
     if (this.totalPercentage < 100 && this.totalPercentage > 0) {
       this.donut_data.datasets[0].data[0] = this.totalPercentage;
       this.donut_data.datasets[0].data[1] = 100 - this.totalPercentage;
+
       if (this.totalPercentage > 70) {
         this.donut_data.datasets[0].backgroundColor[0] = "#b85c5c";
       } else if (this.totalPercentage > 50) {
@@ -124,19 +130,43 @@ export class DashboardComponent implements OnInit {
       } else {
         this.donut_data.datasets[0].backgroundColor[0] = "#5cb85c";
       }
-      this.chart1.chart.update(500);
+      this.chart1.chart.update(300);
     }
   }
 
+  updateBarChart() {
+    this.data.datasets[0].data[0] = this.facePercentage;
+    this.data.datasets[0].data[1] = this.behaviorPercentage;
+    this.chart.chart.update(300);
+  }
+
   initTracking() {
+    this.context = this.canvas.nativeElement.getContext('2d');
+    console.log(this.chart1);
     const objects = new tracking.ObjectTracker(['face']);
-    objects.on('track', function(event) {
+    objects.setInitialScale(3);
+    objects.setStepSize(1);
+    objects.setEdgesDensity(0.1);
+    objects.on('track', (event) => {
+      this.context.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
       if (event.data.length === 0) {
         // No objects were detected in this frame.
+        this.addFacePercentage(-0.5);
+        this.addBehaviorPercentage(-0.5);
+        console.log('nothing happen');
       } else {
-        event.data.forEach(function(rect) {
+        event.data.forEach((rect) => {
           // rect.x, rect.y, rect.height, rect.width
-          this.totalPercentage += 0.5;
+          this.addFacePercentage(1);
+          this.addBehaviorPercentage(1);
+          console.log('this is good');
+          this.context.strokeStyle = '#a64ceb';
+          this.context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+          this.context.font = '11px Helvetica';
+          this.context.fillStyle = "#fff";
+          this.context.fillText('臉部', rect.x + rect.width + 5, rect.y + 11);
+          // this.context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+          // this.context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
         });
       }
     });
@@ -147,17 +177,14 @@ export class DashboardComponent implements OnInit {
     // 設定時鐘每秒更新
     setInterval(() => {
       this.currentTime = new Date();
-      // this.totalPercentage += 1;
-      this.updateDonutChart();
     }, 1000);
 
-    // this.chart1.chart.fillText('60%');
-    /*
+    // 設定chart更新
     setInterval(() => {
-      this.data.datasets[0].data[1]++;
-      this.chart.chart.update();
-    },1000);
-    */
+      this.updateBarChart();
+      this.updateDonutChart();
+    }, 500);
+    // this.chart1.chart.fillText('60%');
 
     // MARK: Video Handling
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -224,5 +251,50 @@ export class DashboardComponent implements OnInit {
       labels: ["62%", "32%", "6%"],
       series: [62, 32, 6]
     });
+  }
+
+  updateDonutText() {
+    const width = this.chart1.chart.chart.width;
+    const height = this.chart1.chart.chart.height;
+    const ctx = this.chart1.chart.chart.ctx;
+
+    // ctx.restore();
+    const fontSize = 6; // (height / 114).toFixed(2);
+    ctx.font = fontSize + "em sans-serif";
+    ctx.textBaseline = "middle";
+
+    const text = Math.round(this.totalPercentage) + "%";
+    const textX = Math.round((width - ctx.measureText(text).width) / 2);
+    const textY = height / 2;
+
+    ctx.clearRect(textX, textY, ctx.measureText(text).width, ctx.measureText(text).height);
+    ctx.fillText(text, textX, textY);
+    // ctx.save();
+  }
+
+  addFacePercentage(num: number) {
+    if (this.facePercentage + num > BAR_MIN && this.facePercentage + num < BAR_MAX) {
+      this.facePercentage += num;
+    }
+  }
+
+  addBehaviorPercentage(num: number) {
+    if (this.behaviorPercentage + num > BAR_MIN && this.behaviorPercentage + num < BAR_MAX) {
+      this.behaviorPercentage += num;
+    }
+  }
+
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    console.log(event.key);
+    if (event.key === 'q') {
+      this.addFacePercentage(10);
+    } else if (event.key === 'a') {
+      this.addFacePercentage(-10);
+    } else if (event.key === 'w') {
+      this.addBehaviorPercentage(10);
+    } else if (event.key === 's') {
+      this.addBehaviorPercentage(-10);
+    }
   }
 }
